d54e3e0bcdabdeadd58eec031039166b32718c49
Shariar Shaikot <mail2shariar@gmail.com>
2018-01-01 21:34:17 +0600

    added basic video and canvas classes

---
+++ src/video.js
@@ -0,0 +1,15 @@
+export default class Video {
+  constructor(uri) {
+    this.node = document.createElement('video');
+    this.node.setAttribute('src', uri);
+    this.node.setAttribute('muted', true);
+    this.node.style.display = 'none';
+    this.node.addEventListener('error', this.error.bind(this));
+    this.node.addEventListener('suspend', this.error.bind(this));
+    this.node.addEventListener('abort', this.error.bind(this));
+  }
+
+  error(event) {
+    throw new Error(`There's an error (${event.type}) occurred`);
+  }
+}
